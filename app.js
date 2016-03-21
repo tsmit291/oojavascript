ba36f8bfa4456e2b29650f861abc5402f00bda97
= <tsmit291@icloud.com>
2016-03-21 11:17:41 -0600

    working with this to create variables for instances

---
+++ app.js
@@ -0,0 +1,8 @@
+function Car(make){
+  this.make = make;
+}
+var prius = new Car('toyota');
+
+console.log(prius);
+// constructors are uppercase, functions are lowercase.
+// TODO: step 1, create a constructor. step 2, create an instance of the constructor. So your constructor is a blueprint. step 3, use this.
