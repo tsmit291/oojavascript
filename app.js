7a3263055fcff67c7c05f6a33bf6be8d342237c2
= <tsmit291@icloud.com>
2016-03-21 11:47:32 -0600

    exercise complete

--- app.js
+++ app.js
@@ -4,8 +4,21 @@ function Car(make,gas,on,mileage){
   this.on = false;
   this.mileage = mileage;
 }
-var prius = new Car('toyota', '10', 'true', '111111');
+Car.prototype.start = function(){
+  this.isOn = true;
+}
+var prius = new Car('toyota', 'prius', '50', 'true', '199999');
 
+prius.start();
 console.log(prius);
+
+Car.prototype.refuel = function(){
+  this.gas = '150';
+}
+
+prius.refuel();
+console.log(prius);
+
 // constructors are uppercase, functions are lowercase.
-// TODO: step 1, create a constructor. step 2, create an instance of the constructor. So your constructor is a blueprint. step 3, use this.
+// TODO: step 1, create a constructor. step 2, create an instance of the constructor. So your constructor is a blueprint. step 3, use this. Makes it easy to read code and create a single blueprint and use it over and over again.
+// TODO: When not to use constructors? This is your personal preference.
